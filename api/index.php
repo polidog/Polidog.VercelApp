0f4f4e241050dcdb4352eaa78e3d6b6811eb20ac
Ryota Mochizuki <mochizuki@ptyhard.co.jp>
2024-09-21 22:18:10 +0900

    test

--- api/index.php
+++ api/index.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use Polidog\VercelApp\Vercel\Bootstrap;
-
-require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())(getenv('VERCEL_ENV') !== 'production' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER));
+//use Polidog\VercelApp\Vercel\Bootstrap;
+//
+//require dirname(__DIR__) . '/autoload.php';
+//exit((new Bootstrap())(getenv('VERCEL_ENV') !== 'production' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER));
+require dirname(__DIR__) . '/public/index.php';
